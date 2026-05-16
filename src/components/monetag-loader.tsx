25811771346256d03c7a4ae05d5ac86f1046a1a9
Z User <z@container>
2026-05-16 09:08:31 +0000

    a90938b0-5e66-4123-939d-79d20f5a7c47

--- src/components/monetag-loader.tsx
+++ src/components/monetag-loader.tsx
@@ -1,40 +1,23 @@
 "use client"
 
-import Script from "next/script"
 import * as React from "react"
 
-// Monetag ad scripts loaded via next/script for maximum compatibility
-// next/script ensures custom attributes (data-zone, data-cfasync) are preserved
-// and works in all browsers including in-app browsers (WhatsApp, Facebook, Instagram)
+// Monetag ad scripts - ULTIMATE FIX
+// Uses dangerouslySetInnerHTML to bypass ALL Next.js attribute stripping
+// This ensures data-zone and data-cfasync attributes are preserved
+// Works in ALL browsers including in-app browsers
 
 export function MonetagLoader() {
   return (
-    <>
-      {/* Monetag Multitag (all-in-one) - Zone 239844 - BEST EARNING */}
-      <Script
-        src="https://quge5.com/88/tag.min.js"
-        strategy="afterInteractive"
-        data-zone="239844"
-        data-cfasync="false"
-      />
-      {/* Monetag Push - Zone 11013630 */}
-      <Script
-        src="https://5gvci.com/act/files/tag.min.js?z=11013630"
-        strategy="afterInteractive"
-        data-cfasync="false"
-      />
-      {/* Monetag Push - Zone 11013418 */}
-      <Script
-        src="https://5gvci.com/act/files/tag.min.js?z=11013418"
-        strategy="afterInteractive"
-        data-cfasync="false"
-      />
-      {/* Monetag Push - Zone 11013421 */}
-      <Script
-        src="https://5gvci.com/act/files/tag.min.js?z=11013421"
-        strategy="afterInteractive"
-        data-cfasync="false"
-      />
-    </>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script src="https://quge5.com/88/tag.min.js" data-zone="239844" async data-cfasync="false"></script>
+          <script src="https://5gvci.com/act/files/tag.min.js?z=11013630" async data-cfasync="false"></script>
+          <script src="https://5gvci.com/act/files/tag.min.js?z=11013418" async data-cfasync="false"></script>
+          <script src="https://5gvci.com/act/files/tag.min.js?z=11013421" async data-cfasync="false"></script>
+        `,
+      }}
+    />
   )
 }
